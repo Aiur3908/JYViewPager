57a4a5487e06c389c3006b9f982e41a721982227
Jerry You <mygoapp.aiur0820@gmail.com>
2018-12-10 11:50:14 +0800

    first commit

---
+++ JYViewPager/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  JYViewPager
+//
+//  Created by 游鴻斌 on 2018/11/30.
+//  Copyright © 2018 JerryYou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
